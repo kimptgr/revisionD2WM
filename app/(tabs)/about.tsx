7fdee5948e3f411a7d29d91012375ef14c316fd8
Kim <patingrekim@gmail.com>
2025-02-09 19:39:56 +0100

    you can navigate and learn but the are a lot of issue

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Application en cours de développement.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightpink",
+  },
+  text: {
+    fontSize: 20,
+  },
+});
