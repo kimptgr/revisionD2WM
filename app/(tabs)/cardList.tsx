43a2965452cb40f60ad33b8b5b2bbf0585463137
kimptgr <patingre.kim@gmail.com>
2025-02-10 11:06:21 +0100

    Add a button, now you can show all or hide all

--- app/(tabs)/cardList.tsx
+++ app/(tabs)/cardList.tsx
@@ -1,19 +1,39 @@
-import { Text, View, StyleSheet, ScrollView, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  FlatList,
+  Pressable,
+} from "react-native";
 import definitions from "../../assets/def.json";
 import SimpleCard from "@/components/SimpleCard";
 import { useState } from "react";
 
 export default function cardList(): JSX.Element {
   const [cards, setCards] = useState(Array(definitions.length).fill(false));
+  const [isAllCardsHidden, setIsAllCardsHidden] = useState(true);
 
   function flipCard(index: number): void {
     const updateCards = cards.slice();
     updateCards[index] = !updateCards[index];
     setCards(updateCards);
   }
+
+  function handleAllCards(): void {
+    setIsAllCardsHidden(!isAllCardsHidden);
+    const updateCards = cards.slice();
+    updateCards.fill(isAllCardsHidden);
+    setCards(updateCards);
+  }
   return (
-    <ScrollView style={styles.backgroundStyle}>
+    <ScrollView contentContainerStyle={styles.backgroundStyle}>
       <Text style={styles.title}>Tout ça à voir 📖</Text>
+      <Pressable style={styles.buttonContainer} onPress={handleAllCards}>
+        <Text style={styles.buttonText}>
+          {isAllCardsHidden ? "Voir tout" : "Cacher tout"}
+        </Text>
+      </Pressable>
       <FlatList
         data={definitions}
         renderItem={({ item, index }) => (
@@ -36,18 +56,32 @@ const styles = StyleSheet.create({
     backgroundColor: "#CCDF92",
     flex: 1,
     flexDirection: "column",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 30,
     fontWeight: "bold",
     marginHorizontal: 16,
-    marginBottom: 48,
+    marginBottom: 24,
   },
   card: {
     marginVertical: 10,
     flex: 1,
     flexDirection: "column",
     alignItems: "center",
+  },
+  buttonContainer: {
+    alignItems: "center",
+    backgroundColor: "#E195AB",
+    borderRadius: 5,
+    padding: 15,
+    width: 300,
+    marginBottom: 24,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
